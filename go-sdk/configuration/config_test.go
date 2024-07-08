a6658539f6e26d96c5c2b659c6c28189cc74fce3
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-07-08 07:53:53 +0700

    feat(sdk): initialize golang sdk

---
+++ go-sdk/configuration/config_test.go
@@ -0,0 +1,54 @@
+package configuration
+
+import (
+	"testing"
+
+	"github.com/Ibrahimsyah/smolf/sdk/env"
+	"github.com/stretchr/testify/assert"
+)
+
+type user struct {
+	FirstName string `yaml:"first_name"`
+	LastName  string `yaml:"last_name"`
+}
+
+type config struct {
+	Hello string `yaml:"hello"`
+	User  user   `yaml:"user"`
+}
+
+func TestReadConfig_SuccessDevelopment(t *testing.T) {
+	var result config
+	err := ReadConfig(ReadConfigParam{
+		Dest:     &result,
+		Folder:   "config",
+		FileName: "configuration",
+		Env:      "development",
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, config{
+		Hello: "world",
+		User: user{
+			FirstName: "john",
+			LastName:  "doe",
+		},
+	}, result)
+}
+
+func TestReadConfig_SuccessProduction(t *testing.T) {
+	var result config
+	err := ReadConfig(ReadConfigParam{
+		Dest:     &result,
+		Folder:   "config",
+		FileName: "configuration",
+		Env:      env.PRODUCTION,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, config{
+		Hello: "world-2",
+		User: user{
+			FirstName: "john",
+			LastName:  "wick",
+		},
+	}, result)
+}
